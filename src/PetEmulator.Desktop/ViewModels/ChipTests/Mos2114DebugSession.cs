6ca31851a28e098d26150b26ab3c99b330aa5793
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2026-09-10 19:05:48 +0200

    Polish chip tester controls

--- src/PetEmulator.Desktop/ViewModels/ChipTests/Mos2114DebugSession.cs
+++ src/PetEmulator.Desktop/ViewModels/ChipTests/Mos2114DebugSession.cs
@@ -4,14 +4,28 @@ namespace PetEmulator.Desktop.ViewModels.ChipTests;
 
 public sealed class Mos2114DebugSession : ChipDebugSessionBase
 {
-    public Mos2114DebugSession()
-        : base("MOS 2114 Color RAM", CreateDefinition()) { }
+    private readonly ColorRamPreviewViewModel _preview;
 
-    private static ChipDebugDefinition CreateDefinition()
+    public Mos2114DebugSession()
+        : this(CreateState()) { }
+
+    private Mos2114DebugSession(Mos2114State state)
+        : base("MOS 2114 Color RAM", state.Definition)
+    {
+        _preview = state.Preview;
+        _preview.Refresh();
+    }
+
+    public override object? Visual => _preview;
+    protected override void AfterRefresh() => _preview?.Refresh();
+
+    private static Mos2114State CreateState()
     {
         var chip = new MOS2114("Chip Tester Color RAM");
-        return new ChipDebugDefinition(
+        return new Mos2114State(new ChipDebugDefinition(
             () => { }, chip.Reset, _ => chip.Read(0), (_, value) => chip.Write(0, value),
-            [("DATA", () => chip.Read(0))], []);
+            [("DATA", () => chip.Read(0))], []), new ColorRamPreviewViewModel(chip));
     }
+
+    private sealed record Mos2114State(ChipDebugDefinition Definition, ColorRamPreviewViewModel Preview);
 }
--- src/PetEmulator.Desktop/Views/ChipTesterView.axaml.cs
+++ src/PetEmulator.Desktop/Views/ChipTesterView.axaml.cs
@@ -1,8 +1,17 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using PetEmulator.Desktop.ViewModels;
 
 namespace PetEmulator.Desktop.Views;
 
 public partial class ChipTesterView : UserControl
 {
     public ChipTesterView() => InitializeComponent();
+
+    private void OnPinToggled(object? sender, RoutedEventArgs e)
+    {
+        if (sender is ToggleSwitch { DataContext: PinRow pin, IsChecked: var level } &&
+            DataContext is ChipTesterViewModel tester && pin.IsWritable)
+            tester.SelectedScenario?.SetPin(pin.Name, level == true);
+    }
 }
