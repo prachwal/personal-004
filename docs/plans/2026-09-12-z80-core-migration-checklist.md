e849a1e7df7b318196abc41621bdd7c52968f12f
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2026-09-13 10:17:51 +0200

    refactor: split Z80 ED opcode registration

--- docs/plans/2026-09-12-z80-core-migration-checklist.md
+++ docs/plans/2026-09-12-z80-core-migration-checklist.md
@@ -29,6 +29,7 @@ Status ogólny: częściowo zakończona. Z80 korzysta już z `CpuProcessorBase` 
 - [x] Zachowano rozszerzenia Z80: IXH/IXL, IYH/IYL, displacement, I/O i nieużywane ED.
 - [x] Główny punkt rejestracji opcode’ów przeniesiono do partiala `Z80Cpu.OpcodeRegistration`.
 - [x] Bazowe opcode’y przeniesiono do `Z80Cpu.OpcodeRegistration.Base`.
+- [x] Opcode’y ED przeniesiono do `Z80Cpu.OpcodeRegistration.Ed`.
 - [ ] Rozdzielić rejestrację opcode’ów na partiale: Base, CB, ED, Indexed i I/O.
 - [ ] Usunąć wrappery `RegisterOpcode`, `RegisterEdOpcode` i `RegisterPageOpcode`, jeśli nie są już potrzebne do kompatybilności.
 - [ ] Ujednolicić metadane `OpcodeDefinition`: mnemonic, długość, tryb adresowania i timing.
@@ -63,6 +64,7 @@ Status ogólny: częściowo zakończona. Z80 korzysta już z `CpuProcessorBase` 
 - [x] Po zmianie typu bazowego wykonano 517 testów jednostkowych Z80 bez testów binarnych; 517/517 przeszło, czas 9 min 11 s.
 - [x] Po przeniesieniu rejestracji opcode’ów do partiala wykonano 517 testów jednostkowych Z80 bez testów binarnych; 517/517 przeszło, czas 9 min 50 s.
 - [x] Po wydzieleniu bazowych opcode’ów wykonano test rozszerzania rejestracji: 1/1, czas 43 ms.
+- [x] Po wydzieleniu opcode’ów ED wykonano 25 testów ED i rozszerzania rejestracji; 25/25 przeszło, czas 1 s.
 - [x] `z80doc.tap` i `zexdoc.com` są obecne w repozytorium.
 - [x] Test ZEXDOC ma inicjalizację wektora stosu CP/M pod `0006h`.
 - [x] Test ZEXDOC raportuje postęp co 100 milionów instrukcji.
---
+++ lib/PetEmulator.CpuZ80/Cpu/Z80Cpu.OpcodeRegistration.Ed.cs
@@ -0,0 +1,94 @@
+using PetEmulator.Core;
+
+namespace PetEmulator.CpuZ80.Cpu;
+
+public partial class Z80Cpu
+{
+    private void RegisterEdOpcodes()
+    {
+        RegisterEdOpcode(0x45, ReturnFromInterrupt);
+        RegisterEdOpcode(0x4D, ReturnFromInterrupt);
+        RegisterEdOpcode(0x55, ReturnFromInterrupt);
+        RegisterEdOpcode(0x5D, ReturnFromInterrupt);
+        RegisterEdOpcode(0x65, ReturnFromInterrupt);
+        RegisterEdOpcode(0x6D, ReturnFromInterrupt);
+        RegisterEdOpcode(0x75, ReturnFromInterrupt);
+        RegisterEdOpcode(0x7D, ReturnFromInterrupt);
+        RegisterEdOpcode(0x47, LoadInterruptRegister);
+        RegisterEdOpcode(0x4F, LoadRefreshRegister);
+        RegisterEdOpcode(0x57, LoadAccumulatorFromInterrupt);
+        RegisterEdOpcode(0x5F, LoadAccumulatorFromRefresh);
+        RegisterEdOpcode(0x56, () => SetInterruptMode(1));
+        RegisterEdOpcode(0x5E, () => SetInterruptMode(2));
+        RegisterEdOpcode(0x67, RotateRightDecimal);
+        RegisterEdOpcode(0x6F, RotateLeftDecimal);
+        RegisterEdOpcode(0xA0, () => BlockTransfer(true, false));
+        RegisterEdOpcode(0xA1, () => BlockCompare(true, false));
+        RegisterEdOpcode(0xA8, () => BlockTransfer(false, false));
+        RegisterEdOpcode(0xA9, () => BlockCompare(false, false));
+        RegisterEdOpcode(0xB0, () => BlockTransfer(true, true));
+        RegisterEdOpcode(0xB1, () => BlockCompare(true, true));
+        RegisterEdOpcode(0xB8, () => BlockTransfer(false, true));
+        RegisterEdOpcode(0xB9, () => BlockCompare(false, true));
+        RegisterEdOpcode(0xA2, () => BlockInput(true, false));
+        RegisterEdOpcode(0xAA, () => BlockInput(false, false));
+        RegisterEdOpcode(0xB2, () => BlockInput(true, true));
+        RegisterEdOpcode(0xBA, () => BlockInput(false, true));
+        RegisterEdOpcode(0xA3, () => BlockOutput(true, false));
+        RegisterEdOpcode(0xAB, () => BlockOutput(false, false));
+        RegisterEdOpcode(0xB3, () => BlockOutput(true, true));
+        RegisterEdOpcode(0xBB, () => BlockOutput(false, true));
+
+        for (var edOpcode = 0; edOpcode <= byte.MaxValue; edOpcode++)
+        {
+            if ((edOpcode & 0xC7) == 0x40)
+            {
+                var inputOpcode = (byte)edOpcode;
+                RegisterEdOpcode(inputOpcode, () => InputRegister((inputOpcode >> 3) & 7));
+            }
+            else if ((edOpcode & 0xC7) == 0x41)
+            {
+                var outputOpcode = (byte)edOpcode;
+                RegisterEdOpcode(outputOpcode, () => OutputRegister((outputOpcode >> 3) & 7));
+            }
+            else if ((edOpcode & 0xCF) == 0x42)
+            {
+                var arithmeticOpcode = (byte)edOpcode;
+                RegisterEdOpcode(arithmeticOpcode, () => AddPairWithCarry(arithmeticOpcode, true));
+            }
+            else if ((edOpcode & 0xCF) == 0x4A)
+            {
+                var arithmeticOpcode = (byte)edOpcode;
+                RegisterEdOpcode(arithmeticOpcode, () => AddPairWithCarry(arithmeticOpcode, false));
+            }
+            else if ((edOpcode & 0xC7) == 0x44)
+            {
+                RegisterEdOpcode((byte)edOpcode, NegateAccumulator);
+            }
+            else if ((edOpcode & 0xC7) == 0x46)
+            {
+                var modeOpcode = (byte)edOpcode;
+                RegisterEdOpcode(modeOpcode, () => SetInterruptMode((byte)(((modeOpcode >> 3) & 3) == 0 ? 0 : 1)));
+            }
+            else if ((edOpcode & 0xCF) == 0x43)
+            {
+                var storeOpcode = (byte)edOpcode;
+                RegisterEdOpcode(storeOpcode, () => StorePairAbsolute(storeOpcode));
+            }
+            else if ((edOpcode & 0xCF) == 0x4B)
+            {
+                var loadOpcode = (byte)edOpcode;
+                RegisterEdOpcode(loadOpcode, () => LoadPairAbsolute(loadOpcode));
+            }
+        }
+
+        RegisterEdOpcode(0x46, () => SetInterruptMode(0));
+        RegisterEdOpcode(0x4E, () => SetInterruptMode(0));
+        RegisterEdOpcode(0x66, () => SetInterruptMode(0));
+        RegisterEdOpcode(0x6E, () => SetInterruptMode(0));
+        RegisterEdOpcode(0x56, () => SetInterruptMode(1));
+        RegisterEdOpcode(0x76, () => SetInterruptMode(1));
+        RegisterEdOpcode(0x5E, () => SetInterruptMode(2));
+        RegisterEdOpcode(0x7E, () => SetInterruptMode(2));
+    }
+}
